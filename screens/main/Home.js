6ff59f271f994a373f5ae4195e65b4a2c9f0f0b9
Vita Koval <mayevit@hotmail.com>
2023-02-15 15:35:06 +0200

    create tab bar and style

--- screens/main/Home.js
+++ screens/main/Home.js
@@ -1,7 +1,7 @@
 // import { useState, useEffect } from "react";
-// import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { TouchableOpacity } from "react-native-web";
+import { Button, TouchableOpacity } from "react-native-web";
 // import { glStyle } from "../../styles/style";
 // screens
 import PostsScreen from "./PostsScreen";
@@ -17,22 +17,53 @@ const MainTab = createBottomTabNavigator();
 
 function Home() {
   return (
-     <MainTab.Navigator screenOptions={{ tabBarShowLabel: false,  }}>
+    <MainTab.Navigator
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: "#FF6C00",
+      }}
+    >
       <MainTab.Screen
         options={{
           tabBarIcon: ({ focused, color, size }) => (
             <Feather name="grid" size={size} color={color} />
           ),
+          // header: ({ navigation, route, options }) => (
+
+          // ),
         }}
         name="Posts"
         component={PostsScreen}
       />
       <MainTab.Screen
+        options={{
+          tabBarIcon: ({ focused, color, size }) => {
+            return (
+              <View
+                style={{
+                  backgroundColor: focused ? "#F6F6F6" : "#FF6C00",
+                  borderRadius: 100,
+                  width: 70,
+                  height: 40,
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                {!focused ? (
+                  <Entypo name="plus" size={size} color="#FFFFFF" />
+                ) : (
+                  <Feather name="trash-2" size={size} color="#BDBDBD" />
+                )}
+              </View>
+            );
+          },
+        }}
         // options={{
-        //   tabBarButton: () => (
-        //     <TouchableOpacity />
-        //     //   <Entypo name="plus" size={13} color="#FFFFFF" />
-        //     // </TouchableOpacity>
+        //   tabBarButton: ()=> (<Text>+</Text>)
+        // }}
+        // options={{
+        //   tabBarButton: ({style}) => (
+        //       <Entypo name="plus" size={13} color="red" />
         //   ),
         // }}
         // options={{
